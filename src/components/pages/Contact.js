4de483255b812c4227ada8629b47a0a830021467
lindsfitz <lindsaytfitz@gmail.com>
2022-01-05 16:59:17 -0800

    adding in emailjs

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from 'emailjs/browser'
 
 export default function Contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [emailSent, setEmailSent] = useState(false);
+
+    const submit = () => {
+        if (name && email && message) {
+            const serviceId = 'service_1pfy7lc';
+            const templateId = 'template_90u2a19';
+            const userId = 'user_DO6rrtHQtz7YolqbTInhc';
+            const templateParams = {
+                name,
+                email,
+                message
+            };
+
+            emailjs.send(serviceId, templateId, templateParams, userId)
+                .then(response => console.log(response))
+                .then(error => console.log(error));
+    
+            setName('');
+            setEmail('');
+            setMessage('');
+            setEmailSent(true);
+        } else {
+            alert('Please fill in all fields.');
+        }
+    }
+
     return (
         <section id="contact-me">
             <h1>Hiring or Have Questions?</h1>
@@ -8,13 +38,21 @@ export default function Contact() {
             <div>
                 <form>
                     <div>
-                        <input type="text" placeholder="Name" />
-                        <input type="text" placeholder="Email" />
+                        <input type="text" placeholder="Name"
+                            defaultValue={name}
+                            onChange={e => setName(e.target.value)} />
+                        <input type="text" placeholder="Email"
+                            defaultValue={email}
+                            onChange={e => setEmail(e.target.value)} />
                         <textarea id="exampleFormControlTextarea1" rows="10"
-                            placeholder="Message"></textarea>
+                            placeholder="Message"
+                            defaultValue={message}
+                            onChange={e => setMessage(e.target.value)}>
+                            </textarea>
                     </div>
                     <div id="btn-div">
-                        <button id="submit" type="button">SEND</button>
+                        <button id="submit" type="button" onClick={submit}
+                        >SEND</button>
                     </div>
                 </form>
             </div>
